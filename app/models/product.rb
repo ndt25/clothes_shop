526e19d6447376ce1249f42041cd35be54c5ce0d
Tho Tran <thotran2981994@gmail.com>
2016-05-28 11:14:16 +0700

    a

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ActiveRecord::Base
-	has_many :order_details
-	has_many :upload_files, foreign_key: 'object_id', dependent: :destroy
-	belongs_to :category
-	belongs_to :color
-	belongs_to :size
+	# Validations
+	
+		validates :name, presence: true, message: 'Tên sản phẩm không được bỏ trống'
+	
+	# / Validations
+
+	# Associations
+	
+		has_many :order_details
+		has_many :upload_files, foreign_key: 'object_id', dependent: :destroy
+		belongs_to :category
+		belongs_to :color
+		belongs_to :size
+	
+	# / Associations
 end
